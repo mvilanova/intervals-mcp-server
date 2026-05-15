951974fba453b514d3fdc1ef6e3f7414dc09de85
laulopezreal <lauureal@gmail.com>
2026-05-15 17:23:11 +0200

    Revert "Update web/proxy.ts"
    This reverts commit 6c27a2dc89f0025dfacac374759cc6b217b1e898.

--- web/proxy.ts
+++ web/proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse, type NextRequest } from "next/server";
 
 const COOKIE = "dashboard_pin";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   if (
